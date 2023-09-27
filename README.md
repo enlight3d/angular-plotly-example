b1b7b434d980ef820291e13a2ad944ff536ad023
enlight3d <thib.974@gmail.com>
2023-09-27 17:41:46 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-plotly-example
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/kuwm7w)
---
+++ src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
+import { ReactiveFormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { PlotComponent } from './plot/plot.component';
+
+import * as PlotlyJS from 'plotly.js-dist-min';
+import { PlotlyModule } from 'angular-plotly.js';
+
+PlotlyModule.plotlyjs = PlotlyJS;
+
+@NgModule({
+  imports: [
+    BrowserModule,
+    ReactiveFormsModule,
+    RouterModule.forRoot([{ path: '', component: PlotComponent }]),
+    PlotlyModule,
+  ],
+  declarations: [AppComponent, PlotComponent],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at https://angular.io/license
+*/
